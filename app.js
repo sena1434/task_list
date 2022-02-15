0b37d1b437fd37404702481a390c529341059ee8
Sena's Account <senacetinkaya.tr@gmail.com>
2022-02-15 22:08:18 +0300

    delete & filter tasks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Define UI Vars
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-task");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
@@ -12,13 +12,22 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
-}
 
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
+}
 // Add Task
 function addTask(e) {
   if (taskInput.value === "") {
     alert("Add a task");
   }
+
   // Create li element
   const li = document.createElement("li");
   // Add class
@@ -41,4 +50,37 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// // Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = "";
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLocaleLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
